38191209e6e31a5a4c5de41fad56a9389e3bacae
masudfcs1 <masudfcs1@gmail.com>
2023-03-22 11:55:44 +0600

    How Work Behind the React

---
+++ s12-How-Work-Behind-the-React/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <Button variant="contained" color="success">
+        Success
+      </Button>
+    </div>
+  );
+}
+
+export default App;
