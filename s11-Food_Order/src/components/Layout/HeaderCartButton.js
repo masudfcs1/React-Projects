20e1cd10b91afc6baaab8a4ce67be3e8274bd47e
masudfcs1 <masudfcs1@gmail.com>
2023-03-22 08:25:19 +0600

    Add useEffect

--- s11-Food_Order/src/components/Layout/HeaderCartButton.js
+++ s11-Food_Order/src/components/Layout/HeaderCartButton.js
@@ -4,16 +4,27 @@ import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 
 export default function HeaderCartButton(props) {
+  const[btnIsHighLighted,setBtnIsHighLighted]=useState(false)
   const cartctx=useContext(CartContext)
-   
-  const numberOfCartItems =cartctx.items.reduce((curNumber,item)=>{
+
+  const {items}=cartctx
+
+  const numberOfCartItems =items.reduce((curNumber,item)=>{
     return curNumber + item.amount;
   },0)
  
-  const btnClasses=`${classes.button} ${bt} `
   
+  const btnClasses=`${classes.button} ${btnIsHighLighted ? classes.bump : ''} `
+  
+  useEffect(()=>{
+    if(items.length === 0){
+      return
+    }
+    setBtnIsHighLighted(true)
+  },[items])
+
   return (
-    <button className={classes.button} onClick={props.onClickkk} >
+    <button className={btnClasses} onClick={props.onClickkk} >
         <span className={classes.icon}>
             <CartIcon/>
         </span>
