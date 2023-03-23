f91240fdfcd70b9fca7646e8a5a47f56bc738266
masudfcs1 <masudfcs1@gmail.com>
2023-03-23 10:14:55 +0600

    Add api

--- s14/src/App.js
+++ s14/src/App.js
@@ -1,34 +1,36 @@
-import React from 'react';
 
+import React, { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+   const[moviest,setmoviesList]=useState([])
 
+   const fetchMoveApiHandler = () => {
+     fetch('https://swapi.dev/api/films').then(res=>{
+      return res.json()
+     }).then((data)=>{
+      const transformmoviestodata=data.results.map(moviedata=>{
+        return {
+          id: moviedata.episode_id,
+          tille: moviedata.title,
+          releaseDate: moviedata.release_data,
+          openingText : moviedata.opening_crawl,
+        }
+      })
+      setmoviesList(transformmoviestodata)
+     })
+   }
+     
+  } 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={moviest} />
       </section>
     </React.Fragment>
   );
 }
-
-export default App;
