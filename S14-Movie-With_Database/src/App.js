cf4927e63a729dab0f548a8af7db12231bba02a4
masudfcs1 <masudfcs1@gmail.com>
2023-03-23 13:14:01 +0600

    Connect to Firebase

--- S14-Movie-With_Database/src/App.js
+++ S14-Movie-With_Database/src/App.js
@@ -13,22 +13,33 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-b8237-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
+      
+      const loadedmovies=[]
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      for(const key in data){
+        loadedmovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedmovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,8 +50,16 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+ async function addMovieHandler(movie) {
+    const res= await fetch('https://react-http-b8237-default-rtdb.firebaseio.com/movies.json',{
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data=await res.json()
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
