c437d1f24d1e5ae0d696b5a52d9a9b946dd683b7
masudfcs1 <masudfcs1@gmail.com>
2023-03-23 11:53:17 +0600

    Error and Condition

--- s14/src/App.js
+++ s14/src/App.js
@@ -1,41 +1,68 @@
-
-import React, { useState } from 'react';
-import MoviesList from './components/MoviesList';
-import './App.css';
+import React, { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-   const[movies,setmoviesList]=useState([])
-   const[isLoading, setLoading]=useState(false)
+  const [movies, setmoviesList] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, seterror] = useState(null);
 
- async  function fetchMoveApiHandler() {
-    setLoading(true) 
-  const res=await fetch('https://swapi.dev/api/films')
-      const data= await res.json()
-          
-      const transformmoviestodata=data.results.map(moviedata=>{
+  async function fetchMoveApiHandler() {
+    setLoading(true);
+    seterror(null)
+
+
+
+    try{
+         const res = await fetch("https://swapi.dev/api/films");
+      if(!res.ok){
+        throw new Error('Something Went Wrong!')
+      }
+  
+      const data = await res.json();
+
+      const transformmoviestodata = data.results.map((moviedata) => {
         return {
           id: moviedata.episode_id,
           releaseDate: moviedata.release_data,
           title: moviedata.title,
-          openingText : moviedata.opening_crawl,
-        }
-      })
-      setmoviesList(transformmoviestodata)
-       setLoading(false)
-   }
+          openingText: moviedata.opening_crawl,
+        };
+      });
+
+      setmoviesList(transformmoviestodata);
+    //  setLoading(false);
+    }
+    catch(error){
+      seterror(error.message)
      
-  
+    }
+     setLoading(false);
+  }
+
+  let content=<p>Found No Movies</p>
+
+  if(movies.length>0){
+    content= <MoviesList movies={movies}/>
+  }
+
+  if(error){
+    content=<p>{error}</p>
+  }
+
+  if(isLoading){
+    content=<p>Loading...</p>
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoveApiHandler}>Fetch Movies</button>
       </section>
       <section>
-     {!isLoading &&  movies.length>0 && <MoviesList movies={movies} />}
-     {!isLoading && movies.length==0 && <p>No Movies Details</p>}
-     {isLoading && <p>Loading...</p>}
+       {content}
       </section>
     </React.Fragment>
   );
 }
-export default App
+export default App;
