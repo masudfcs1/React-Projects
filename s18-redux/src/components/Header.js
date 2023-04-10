777a9526a60c7d4637b261df6a03eb1c07ccabc2
masudfcs1 <masudfcs1@gmail.com>
2023-04-10 15:38:21 +0600

    Split Code

--- s18-redux/src/components/Header.js
+++ s18-redux/src/components/Header.js
@@ -1,9 +1,19 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Header.module.css';
+import { authActions } from '../store/auth';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
-      <h1>Redux Auth</h1>
+    <h1>Redux Auth</h1>
+    {isAuth && (
       <nav>
         <ul>
           <li>
@@ -13,11 +23,12 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
       </nav>
-    </header>
+    )}
+  </header>
   );
 };
 
--- s18-redux/src/store/index.js
+++ s18-redux/src/store/index.js
@@ -1,39 +1,56 @@
-import { createStore } from 'redux'
+// import { createStore } from "redux";
+// import createSlice from '@reduxjs/toolkit'
 
-const initialState ={counter: 0, showCounter: true}
-const counterReducer = (state=initialState ,action) => {
-    
-    if(action.type ==='increment'){
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        }
-    }
-    
-    if(action.type ==='increase'){
-        return {
-            counter: state.counter + action.value,
-            showCounter: state.showCounter
-        }
-    }
+// const initialState = { counter: 0, showCounter: true };
 
-     if(action.type ==='decrement'){
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-    if(action.type ==='toggle'){
-        return {
-    
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
-    
-    return state
-}
+// const counterSlice =createSlice({
+//     name : 'counter',
+   
+// })
 
-const store = createStore(counterReducer)
+// // const counterReducer = (state = initialState, action) => {
+// //   if (action.type === "increment") {
+// //     return {
+// //       counter: state.counter + 1,
+// //       showCounter: state.showCounter,
+// //     };
+// //   }
 
-export default store
+// //   if (action.type === "increase") {
+// //     return {
+// //       counter: state.counter + action.value,
+// //       showCounter: state.showCounter,
+// //     };
+// //   }
+
+// //   if (action.type === "decrement") {
+// //     return {
+// //       counter: state.counter - 1,
+// //       showCounter: state.showCounter,
+// //     };
+// //   }
+// //   if (action.type === "toggle") {
+// //     return {
+// //       showCounter: !state.showCounter,
+// //       counter: state.counter,
+// //     };
+// //   }
+
+// //   return state;
+// // };
+
+// const store = createStore(counterReducer);
+
+// export default store;
+
+import { configureStore } from '@reduxjs/toolkit';
+
+import counterReducer from './counter';
+import authReducer from './auth';
+
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
+
+export default store;
