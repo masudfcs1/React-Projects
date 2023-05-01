40f9a2437fee02c46168e34801343c788a228534
masudfcs1 <masudfcs1@gmail.com>
2023-05-01 09:12:33 +0600

    Multipule Pages

--- Routing/20.1/src/App.js
+++ Routing/20.1/src/App.js
@@ -1,10 +1,25 @@
-import {createBrowerRouter} from 'react-router-dom'
-import Homepages from './components/Homepages';
-import productpage from './components/productpage';
+import React from 'react'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Homepages from './components/Homepages'
+import Productpage from './components/Productpage'
+import Aboutpages from './components/Aboutpages'
 
+const router=createBrowserRouter([
+  {path:'/',element: <Homepages/> },
+  {path:'/products',element: <Productpage/> },
+  {path:'/products',element: <Productpage/> },
+  {path:'/about',element: <Aboutpages/> },
+  {path:'/products',element: <Productpage/> },
+  {path:'/products',element: <Productpage/> },
+])
 
-function App() {
-  return <div> Home page </div>;
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
 }
 
-export default App;
+
+export default App
