e0eb5bbf50709a0a54dbfd0192b2e95dd8af2a39
masudfcs1 <masudfcs1@gmail.com>
2023-05-02 07:26:45 +0600

    Use Link

--- Routing/20.1/src/components/Homepages.jsx
+++ Routing/20.1/src/components/Homepages.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Homepages = () => {
   return (
     <div>
       <h1>Home Page!</h1>
+      <Link to='/products'>The list of Products</Link><br></br>
+      <Link to='/about'>The list of about</Link>
     </div>
   )
 }
